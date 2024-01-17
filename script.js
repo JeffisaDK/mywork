07b634848a62caf3d867ed39a553cb494c3c00c0
JeffisaDK <j.delaosia@gmail.com>
2024-01-17 22:35:28 +0800

    6 contents submitted

--- script.js
+++ script.js
@@ -5,11 +5,27 @@ function showportfolio() {
     let portobtn = document.getElementById("portfoliobtn");
     let aboutbtn = document.getElementById("aboutbtn");
 
+    let card1 = document.getElementById("content1");
+    let card2 = document.getElementById("content2");
+    let card3 = document.getElementById("content3");
+    let card4 = document.getElementById("content4");
+    let card5 = document.getElementById("content5");
+    let card6 = document.getElementById("content6");
+
     portoscreen.style.display = "block";
     portobtn.className = "active1";
 
     aboutscreen.style.display = "none";
     aboutbtn.className = "menu2";
+    
+    card1.style.display = "none";
+    card2.style.display = "none";
+    card3.style.display = "none";
+    card4.style.display = "none";
+    card5.style.display = "none";
+    card6.style.display = "none";
+
+    window.scrollTo(0,0);
 }
 
 function showabout() {
@@ -19,9 +35,139 @@ function showabout() {
     let portobtn = document.getElementById("portfoliobtn");
     let aboutbtn = document.getElementById("aboutbtn");
 
+    let card1 = document.getElementById("content1");
+    let card2 = document.getElementById("content2");
+    let card3 = document.getElementById("content3");
+    let card4 = document.getElementById("content4");
+    let card5 = document.getElementById("content5");
+    let card6 = document.getElementById("content6");
+
     aboutscreen.style.display = "block";
     aboutbtn.className = "active2";
 
     portoscreen.style.display = "none";
     portobtn.className = "menu1";
+    
+    card1.style.display = "none";
+    card2.style.display = "none";
+    card3.style.display = "none";
+    card4.style.display = "none";
+    card5.style.display = "none";
+    card6.style.display = "none";
+
+    window.scrollTo(0,0);
+}
+
+function showcontent1 () {
+    let portoscreen = document.getElementById("portfolio");
+    let aboutscreen = document.getElementById("about");
+
+    let portobtn = document.getElementById("portfoliobtn");
+    let aboutbtn = document.getElementById("aboutbtn");
+    
+    let card1 = document.getElementById("content1");
+
+    card1.style.display = "block";
+    portoscreen.style.display = "none";
+    aboutscreen.style.display = "none";
+
+    portobtn.className = "menu1";
+    aboutbtn.className = "menu2";
+
+    window.scrollTo(0,0);
+}
+
+function showcontent2 () {
+    let portoscreen = document.getElementById("portfolio");
+    let aboutscreen = document.getElementById("about");
+
+    let portobtn = document.getElementById("portfoliobtn");
+    let aboutbtn = document.getElementById("aboutbtn");
+    
+    let card2 = document.getElementById("content2");
+
+    card2.style.display = "block";
+    portoscreen.style.display = "none";
+    aboutscreen.style.display = "none";
+
+    portobtn.className = "menu1";
+    aboutbtn.className = "menu2";
+
+    window.scrollTo(0,0);
+}
+
+function showcontent3 () {
+    let portoscreen = document.getElementById("portfolio");
+    let aboutscreen = document.getElementById("about");
+
+    let portobtn = document.getElementById("portfoliobtn");
+    let aboutbtn = document.getElementById("aboutbtn");
+    
+    let card3 = document.getElementById("content3");
+
+    card3.style.display = "block";
+    portoscreen.style.display = "none";
+    aboutscreen.style.display = "none";
+
+    portobtn.className = "menu1";
+    aboutbtn.className = "menu2";
+
+    window.scrollTo(0,0);
+}
+
+function showcontent4 () {
+    let portoscreen = document.getElementById("portfolio");
+    let aboutscreen = document.getElementById("about");
+
+    let portobtn = document.getElementById("portfoliobtn");
+    let aboutbtn = document.getElementById("aboutbtn");
+    
+    let card4 = document.getElementById("content4");
+
+    card4.style.display = "block";
+    portoscreen.style.display = "none";
+    aboutscreen.style.display = "none";
+
+    portobtn.className = "menu1";
+    aboutbtn.className = "menu2";
+
+    window.scrollTo(0,0);
+}
+
+function showcontent5 () {
+    let portoscreen = document.getElementById("portfolio");
+    let aboutscreen = document.getElementById("about");
+
+    let portobtn = document.getElementById("portfoliobtn");
+    let aboutbtn = document.getElementById("aboutbtn");
+    
+    let card5 = document.getElementById("content5");
+
+    card1.style.display = "block";
+    portoscreen.style.display = "none";
+    aboutscreen.style.display = "none";
+
+    portobtn.className = "menu1";
+    aboutbtn.className = "menu2";
+
+    window.scrollTo(0,0);
+}
+
+function showcontent6 () {
+    let portoscreen = document.getElementById("portfolio");
+    let aboutscreen = document.getElementById("about");
+
+    let portobtn = document.getElementById("portfoliobtn");
+    let aboutbtn = document.getElementById("aboutbtn");
+    
+    let card6 = document.getElementById("content6");
+
+    card1.style.display = "block";
+    portoscreen.style.display = "none";
+    aboutscreen.style.display = "none";
+
+    portobtn.className = "menu1";
+    aboutbtn.className = "menu2";
+
+    window.scrollTo(0,0);
 }
